45217e25e7ce7791466d02fce1db0f65e4684cf3
Alyssa Choi <alyssa.seoyoon@gmail.com>
2021-05-08 23:25:14 -0400

    added requirements.txt

---
+++ flask-backend/static/react/precache-manifest.8de1fa90dc8573a36ea03c5b79ed75e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8acb96e82b50f5e5d3c691cd9c49d5ce",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "f61360c2c39cd44780c0",
+    "url": "/static/react/css/main.1ed1209b.chunk.css"
+  },
+  {
+    "revision": "eff6693dc1edba8fcc0a",
+    "url": "/static/react/js/2.5f863279.chunk.js"
+  },
+  {
+    "revision": "a8de4128b679d6159aad8871b5569c6d",
+    "url": "/static/react/js/2.5f863279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f61360c2c39cd44780c0",
+    "url": "/static/react/js/main.8e904337.chunk.js"
+  },
+  {
+    "revision": "e19b87ea591ba9a4e41c",
+    "url": "/static/react/js/runtime-main.ab13116e.js"
+  }
+]);
